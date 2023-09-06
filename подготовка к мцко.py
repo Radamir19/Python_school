9a31f551eac70718f8459c97478f5d58b597864d
Radamir19 <radamir33@mail.ru>
2023-09-06 07:42:46 +0300

    дз на 6.09

--- подготовка к мцко.py
+++ подготовка к мцко.py
@@ -1,3 +1,5 @@
+from random import randint
+from time import time
 # task 2
 # for x in 0, 1:
 #     for y in 0, 1:
@@ -110,6 +112,150 @@
 #         return 1
 #     return resolve(x - 1, y) + resolve((x % 2), y)
 #
-# print(resolve(30, 12) * resolve(12, 1)).
+# print(resolve(30, 12) * resolve(12, 1))
 
 
+# task 4
+# for i in range(1000):
+#     n = bin(i)[2:]
+#     if i % 2 == 0:
+#         n += '10'
+#     else:
+#         n += '11'
+#     if n.count('1') % 2 == 0:
+#         n += n[-1]
+#     else:
+#         n += n[-2]
+#     R = int(n, 2)
+#     if R > 44:
+#         print(i)
+#         break
+
+# d = 1
+# while d < 40:
+#     p = 5
+#     q = 7
+#     e = 11
+#     f_n = (p - 1) * (q - 1)
+#     if (d * e) % f_n == 1:
+#         print(d)
+#     d += 1
+
+# print((~18 | (132 >> 2)) & (86 << 1))
+
+
+# def resolve(x, y):
+#     if x > y or x == 32:
+#         return 0
+#     if x == y:
+#         return 1
+#     return resolve(x + 3, y) + resolve(x + 4, y) + resolve(x * 3, y)
+#
+# print(resolve(4, 16) * resolve(16, 46))
+
+# k = 0
+# for i in range(1000):
+#     s = i
+#     n = 120
+#     while s > 0:
+#         s //= 6
+#         n -= 6
+#     if i % 2 == 0 and n == 108:
+#         k += 1
+# print(k)
+
+# a = [int(i) for i in open('13.txt')]
+# res = []
+# for i in range(len(a) - 1):
+#     if (a[i] % 8 == 0 and a[i + 1] % 8 == 0):
+#         res.append(a[i] + a[i + 1])
+# print(len(res), min(res))
+
+# N = int(input())
+# K = int(input())
+# P = int(input())
+# U = int(input())
+#
+# d = abs(P - U)
+# t = divmod(d, K)
+# j = P % K if P > U < K else (N - P) % K if P < U > N - K else K
+# print(min(sum(t), 1 + j + int.__sub__(*t)))
+
+
+# from math import prod, isqrt
+# a, r1, _ = sides = tuple(map(int, map(input, ('',) * 3)))
+# p = sum(sides)
+# sq = prod(map(int.__sub__, (p,) * 4, map((2).__mul__, (0,) + sides))) // (4 * a * a)
+# print(isqrt(r1 * r1 - sq), -isqrt(sq), sep = '\n')
+
+#
+# N = int(input())
+# K = int(input())
+# p = []
+# for i in range(1, N + 1):
+#     p.append(i)
+# st = 0
+# isFound = False
+# while len(p) > 0:
+#     st += 1
+#     if p[0] == K:
+#         isFound = True
+#         break
+#     p.pop(0)
+#     if p[0] == K:
+#         isFound = False
+#         st += 1
+#         break
+#     p.pop(0)
+#     st += 1
+#     p.append(p.pop(0))
+#     st += 1
+#     if len(p) % 2 == 0:
+#         st += len(p)
+#         break
+#     else:
+#         st += len(p) + 1
+#         break
+
+
+
+# n = int(input())
+# k = int(input())
+# p = []
+# for i in range(1, n + 1):
+#     p.append(i)
+# st = 0
+# isFound = False
+# while len(p) > 0:
+#     if p[0] == k:
+#         isFound = True
+#         st += 1
+#         break
+#     p.pop(0)
+#     st += 1
+#     if p[0] == k:
+#         isFound = False
+#         st += 1
+#         break
+#     p.pop(0)
+#     st += 1
+#     p.append(p.pop(0))
+#     st += 1
+# if isFound == True:
+#     print("Yes")
+#     print(st)
+# else:
+#     print("No")
+#     print(st)
+
+n = int(input())
+k = 0
+t1 = time()
+for i in range(n):
+    if i % 2 == 0:
+        x = bin(i)[2:]
+        if x.count('1') % 2 == 0:
+            k += 1
+t2 = time()
+print(t2 - t1)
+print(k)
